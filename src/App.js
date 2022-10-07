c9f8165cd1395d0782eb989ebeace63dbe5b557d
stephen4018 <stephenohaeri4018@gmail.com>
2022-10-07 05:52:59 +0100

    splitted components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.scss";
+import "./styles/App.scss";
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import React from "react";
+import ExpenseDate from "./ExpenseDate";
 
-const ExpenseItem = (props) => {
+const ExpenseItem = ({ title, amount, date }) => {
   return (
     <div className="expense-item">
-      <div>{props.date.toIsoString()}</div>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">{amount}</div>
       </div>
     </div>
   );
