57ee91316ee5c240f07963d486c6d71a5ec184f7
stephen4018 <stephenohaeri4018@gmail.com>
2022-10-10 23:43:19 +0100

    introduced the useState hook to the app

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = ({ title, amount, date }) => {
+  const [head, setHead] = useState(title);
+
+  const changeHead = () => {
+    setHead(!head);
+  };
+
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{head}</h2>
         <div className="expense-item__price">{amount}</div>
       </div>
-    </div>
+      <button onClick={changeHead}>click</button>
+    </Card>
   );
 };
 
